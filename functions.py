3cde3ff3759f47d198bd27e523ea08c441f8b7bc
Axel Puech <axel75puech@gmail.com>
2023-01-22 17:37:35 +0100

    working with open cv

--- functions.py
+++ functions.py
@@ -1,45 +1,13 @@
 import cv2
 import numpy as np
 
-def stack_images(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
 
+# class image_prediction:
+
+#     def __init__(self):
+        
 
 def return_resized_img_percent(img, percent):
-
-
-    # height, width, channels = img.shape
-    # print(f'Width: {width}, Height: {height}')
-
-
 
     width = int(img.shape[1] * percent / 100)
     height = int(img.shape[0] * percent / 100)
@@ -84,15 +52,6 @@ def biggestContour(contours):
                 max_area = area
     return biggest,max_area
 
-def drawRectangle(img,biggest,thickness):
-    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
-    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
-    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
-    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
- 
-    return img
-
-
 def extract_points(pts):
     points_list = []
 
@@ -103,29 +62,6 @@ def extract_points(pts):
         points_list.append(int_list)
 
     return points_list
-
-
-def getCountours(img, imgCountours):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-
-        if area > 6000:
-            # cv2.drawContours(imgCountours, cnt, -1, (255,0,255), 7)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-
-            x, y , w, h = cv2.boundingRect(approx)
-
-            cv2.rectangle(imgCountours, (x,y), (x+w,y+h), (0,255,0), 5)
-
-            cv2.putText(imgCountours, " card", (x + w + 10, y + 10), cv2.FONT_HERSHEY_COMPLEX, 0.5 , (0,255,0), 2 )
-            card = imgCountours[y:y + h, x:x + w]
-            card_resized = return_resized_img_size(card)
-
-
-    return card_resized, contours, imgCountours
 
 def add_card_name(image, card_name, points_list):
 
--- shape_detector_img.py
+++ shape_detector_img.py
@@ -12,40 +12,28 @@ dict_trained_path = 'archive\\dict_black_white_244_padded (2).pkl'
 
 predictor = Predict(model_trained_path,dict_trained_path,threshold,interval)
 
-#pour chaque image dans le fichier image
-for file in os.listdir('images'):
- 
-    img = cv2.imread("images\\" +file)
+
+
+def plot_card_and_name(path_image):
+
+    img = cv2.imread(path_image)
     resized,width, height = functions.return_resized_img_percent(img, 20)
-
     card_plus_name, _, _=functions.return_resized_img_percent(img, 40)
-
-    imgContour =resized.copy()
-
-    
     original = img.copy()
+
+
     height_original,width_original = original.shape[:2]
-    
-    imgContours2 = resized.copy()
-    
+        
     """ Processing for card detection - resized """
     imgDil = functions.transform_for_contour_detection(resized)
     # this function get the contours and create a pproximative bounding box
-    card,contours, imgcountours = functions.getCountours(imgDil, imgContour)
+    contours, _ = cv2.findContours(imgDil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     # this fucntion is used to scan the card 
-    biggest, maxArea = functions.biggestContour(contours)
-    
-    # """ Processing for card detection - original size """
-    # imgDil_original = functions.transform_for_contour_detection(original)
-    # # this function get the contours and create a pproximative bounding box
-    # _,contours_original, imgcountours_original = functions.getCountours(imgDil_original, imgContour)
-    # # this fucntion is used to scan the card 
-    # biggest, maxArea = functions.biggestContour(contours)
+    biggest, _ = functions.biggestContour(contours)
 
 
     if biggest.size !=0:
         biggest=functions.reorder(biggest)
-        imgContours2 = functions.drawRectangle(imgContours2,biggest, 2)
 
         #get the size of the img and the 4 points at the corner to prepare the scan
         points = np.int32(biggest)
@@ -55,40 +43,23 @@ for file in os.listdir('images'):
         pts2 = np.float32([[0, 0],[width, 0], [0, height],[width, height]])
         pts2_original = pts2 * 5
  
-
-
         # get a list of the 4 points a the corner 
         point_list = functions.extract_points(points)        
-        point_list_original_size = [[x * 5 for x in sublist] for sublist in point_list]
-        
-        imgContours2 = functions.draw_circles_corner(imgContours2, point_list)
-        original = functions.draw_circles_corner(original, point_list_original_size)
-        
-        matrix = cv2.getPerspectiveTransform(pts1, pts2)
-        matrix_original = cv2.getPerspectiveTransform(pts1_original, pts2_original)
-        
-        
-        imgWarpColored = cv2.warpPerspective(resized, matrix, (width, height))
+
+        matrix_original = cv2.getPerspectiveTransform(pts1_original, pts2_original)              
         imgWarpColored_original = cv2.warpPerspective(original, matrix_original, (width_original, height_original))
 
     card_name = predictor.predict_card_name(imgWarpColored_original)
-    print(card_name[0][0]  )
+    print(card_name[0][0])
 
     point_for_card_plus_name = [[x * 2 for x in sublist] for sublist in point_list]
     card_plus_name = functions.add_card_name(card_plus_name, card_name[0][0],point_for_card_plus_name)
     cv2.imshow('card_plus_name', card_plus_name)
     cv2.waitKey(0)
-
-
-    # imgContours2 = functions.return_resized_img_size(imgContours2)
-    # imgWarpColored = functions.return_resized_img_size(imgWarpColored)
-    # imgcountours = functions.return_resized_img_size(imgcountours)
-
- 
-    # imgStack = functions.stack_images(0.8, [imgContours2, imgWarpColored, imgcountours])
-    # cv2.imwrite('image_croped\\'+file+'_croped.jpg', imgWarpColored_original)
-    
-    # # cv2.imshow('Result', imgContour)
-    # cv2.imshow('Result', imgStack)
-    # cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+#pour chaque image dans le fichier image
+for file in os.listdir('images'):
+    path_image = "images\\" +file
+    plot_card_and_name(path_image)
+ 

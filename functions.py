5e5cb90299a4752ef99eaceafb462362b0e3bef0
Axel Puech <axel75puech@gmail.com>
2023-01-23 10:44:49 +0100

    working with card detector class

--- functions.py
+++ functions.py
@@ -2,119 +2,132 @@ import cv2
 import numpy as np
 
 
-# class image_prediction:
+class Card_detection:
 
-#     def __init__(self):
-        
+    def __init__(self, color_text=(0,255,0), color_rectangle=(0,255,0)):
+        self.color_rectangle = color_rectangle
+        self.color_text = color_text
+        # print("card detector created")
 
-def return_resized_img_percent(img, percent):
+    def return_resized_img_percent(self,img, percent):
 
-    width = int(img.shape[1] * percent / 100)
-    height = int(img.shape[0] * percent / 100)
-    dim = (width, height)
-    # resize image
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    return resized, width, height
-
-
-def return_resized_img_size(img, height = 614 , width = 421):
-    resized_image = cv2.resize(img, (width, height), interpolation=cv2.INTER_LINEAR)
-    return resized_image
+        width = int(img.shape[1] * percent / 100)
+        height = int(img.shape[0] * percent / 100)
+        dim = (width, height)
+        # resize image
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        return resized, width, height
 
 
+    def return_resized_img_size(self,img, height = 614 , width = 421):
+        resized_image = cv2.resize(img, (width, height), interpolation=cv2.INTER_LINEAR)
+        return resized_image
 
+    def reorder(self,myPoints):
+    
+        myPoints = myPoints.reshape((4, 2))
+        myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
+        add = myPoints.sum(1)
+    
+        myPointsNew[0] = myPoints[np.argmin(add)]
+        myPointsNew[3] =myPoints[np.argmax(add)]
+        diff = np.diff(myPoints, axis=1)
+        myPointsNew[1] =myPoints[np.argmin(diff)]
+        myPointsNew[2] = myPoints[np.argmax(diff)]
+    
+        return myPointsNew
 
+    def biggestContour(self,contours):
+        biggest = np.array([])
+        max_area = 0
+        for i in contours:
+            area = cv2.contourArea(i)
+            if area > 5000:
+                peri = cv2.arcLength(i, True)
+                approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+                if area > max_area and len(approx) == 4:
+                    biggest = approx
+                    max_area = area
+        return biggest,max_area
 
-def reorder(myPoints):
- 
-    myPoints = myPoints.reshape((4, 2))
-    myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
-    add = myPoints.sum(1)
- 
-    myPointsNew[0] = myPoints[np.argmin(add)]
-    myPointsNew[3] =myPoints[np.argmax(add)]
-    diff = np.diff(myPoints, axis=1)
-    myPointsNew[1] =myPoints[np.argmin(diff)]
-    myPointsNew[2] = myPoints[np.argmax(diff)]
- 
-    return myPointsNew
+    def extract_points(self,pts):
+        points_list = []
 
-def biggestContour(contours):
-    biggest = np.array([])
-    max_area = 0
-    for i in contours:
-        area = cv2.contourArea(i)
-        if area > 5000:
-            peri = cv2.arcLength(i, True)
-            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
-            if area > max_area and len(approx) == 4:
-                biggest = approx
-                max_area = area
-    return biggest,max_area
+        for i in pts:
+            str1 = ' '.join(str(e) for e in i[0])
+            split_list = str1.split(" ", 2)
+            int_list = [int(i) for i in split_list]
+            points_list.append(int_list)
 
-def extract_points(pts):
-    points_list = []
+        return points_list
 
-    for i in pts:
-        str1 = ' '.join(str(e) for e in i[0])
-        split_list = str1.split(" ", 2)
-        int_list = [int(i) for i in split_list]
-        points_list.append(int_list)
+    def add_card_name(self,image, card_name, points_list):
 
-    return points_list
-
-def add_card_name(image, card_name, points_list):
-
-    print("point list", points_list)
-    cv2.putText(image, card_name, (points_list[0][0]+15,points_list[0][1]-15), cv2.FONT_HERSHEY_COMPLEX, 0.5 , (0,255,0), 2 )
-    cv2.line(image, points_list[0],points_list[2], (0,255,0),thickness=3,lineType=2)
-    cv2.line(image, points_list[2],points_list[3], (0,255,0),thickness=3,lineType=2)
-    cv2.line(image, points_list[1],points_list[0], (0,255,0),thickness=3,lineType=2)
-    cv2.line(image, points_list[3],points_list[1], (0,255,0),thickness=3,lineType=2)
-
-    return image
-
-
-
-
-def transform_for_contour_detection(resized):
-    # add blur 
-    imgBlur = cv2.GaussianBlur(resized, (7,7), 1) 
-
-    # convert image into greyscale 
-    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-
-    imgCanny = cv2.Canny(imgGray, 20, 255)
-
-    kernel = np.ones((5,5))
-    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-
-    return imgDil
-
-def draw_circles_corner(img, points_list, color = (0,255,0), radius = 3, thickness = 3 ):
-    # draw circle at the corner of the rectangle
-    for point in points_list:
-        x = point[0]
-        y = point[1]
-        cv2.circle(img,(x,y), radius, color, thickness)
-        
-    return img
-
-def draw_corners_and_lines(image, screenCnt, lineColor = (0,255,0)):
-        x0,y0 = screenCnt[0][0]
-        x1,y1 = screenCnt[1][0]
-        x2,y2 = screenCnt[2][0]
-        x3,y3 = screenCnt[3][0]
-
-        cv2.circle(image,(x0,y0), 3, lineColor, 2)
-        cv2.circle(image,(x1,y1), 3, lineColor, 2)
-        cv2.circle(image,(x2,y2), 3, lineColor, 2)
-        cv2.circle(image,(x3,y3), 3, lineColor, 2)
-
-
-        cv2.line(image, (x0,y0),(x1,y1), lineColor,thickness=2,lineType=2)
-        cv2.line(image, (x1,y1),(x2,y2), lineColor,thickness=2,lineType=2)
-        cv2.line(image, (x2,y2),(x3,y3), lineColor,thickness=2,lineType=2)
-        cv2.line(image, (x3,y3),(x0,y0), lineColor,thickness=2,lineType=2)
+        cv2.putText(image, card_name, (points_list[0][0]+15,points_list[0][1]-15), cv2.FONT_HERSHEY_COMPLEX, 0.5 , self.color_text, 2 )
+        cv2.line(image, points_list[0],points_list[2], self.color_rectangle,thickness=3,lineType=2)
+        cv2.line(image, points_list[2],points_list[3], self.color_rectangle,thickness=3,lineType=2)
+        cv2.line(image, points_list[1],points_list[0], self.color_rectangle,thickness=3,lineType=2)
+        cv2.line(image, points_list[3],points_list[1], self.color_rectangle,thickness=3,lineType=2)
 
         return image
+
+    def transform_for_contour_detection(self,resized):
+        # add blur 
+        imgBlur = cv2.GaussianBlur(resized, (7,7), 1) 
+
+        # convert image into greyscale 
+        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+        imgCanny = cv2.Canny(imgGray, 20, 255)
+
+        kernel = np.ones((5,5))
+        imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+
+        return imgDil
+
+    def return_scaned_card(self,path_image):
+
+        img = cv2.imread(path_image)
+        #we first reduce the size of the image to 20%
+        resized,width, height = self.return_resized_img_percent(img, 20)
+
+        #this image will be the image plotted 
+        self.card_plus_name, _, _=self.return_resized_img_percent(img, 40)
+
+        height_original,width_original = img.shape[:2]
+            
+        """ Processing for card detection - resized """
+        imgDil = self.transform_for_contour_detection(resized)
+        # this function get the contours and create a pproximative bounding box
+        contours, _ = cv2.findContours(imgDil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # this fucntion is used to scan the card 
+        biggest, _ = self.biggestContour(contours)
+
+        if biggest.size !=0:
+            biggest=self.reorder(biggest)
+
+            #get the size of the img and the 4 points at the corner to prepare the scan
+            points = np.int32(biggest)
+            pts1 = np.float32(biggest)
+            pts1_original = pts1*5
+            
+            pts2 = np.float32([[0, 0],[width, 0], [0, height],[width, height]])
+            pts2_original = pts2 * 5
+    
+            # get a list of the 4 points a the corner 
+            self.point_list = self.extract_points(points)        
+
+            matrix_original = cv2.getPerspectiveTransform(pts1_original, pts2_original)              
+            imgWarpColored_original = cv2.warpPerspective(img, matrix_original, (width_original, height_original))
+
+        return imgWarpColored_original
+
+
+    def card_prediction(self, card_name):
+
+        point_for_card_plus_name = [[x * 2 for x in sublist] for sublist in self.point_list]
+        self.card_plus_name = self.add_card_name(self.card_plus_name, card_name[0][0],point_for_card_plus_name)
+        cv2.imshow('card_plus_name', self.card_plus_name)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
--- shape_detector_img.py
+++ shape_detector_img.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import functions
+from functions import Card_detection
 import os
 from predict_functions import Predict
 from keras import backend as K
@@ -12,54 +12,13 @@ dict_trained_path = 'archive\\dict_black_white_244_padded (2).pkl'
 
 predictor = Predict(model_trained_path,dict_trained_path,threshold,interval)
 
+card_detector = Card_detection()
 
-
-def plot_card_and_name(path_image):
-
-    img = cv2.imread(path_image)
-    resized,width, height = functions.return_resized_img_percent(img, 20)
-    card_plus_name, _, _=functions.return_resized_img_percent(img, 40)
-    original = img.copy()
-
-
-    height_original,width_original = original.shape[:2]
-        
-    """ Processing for card detection - resized """
-    imgDil = functions.transform_for_contour_detection(resized)
-    # this function get the contours and create a pproximative bounding box
-    contours, _ = cv2.findContours(imgDil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    # this fucntion is used to scan the card 
-    biggest, _ = functions.biggestContour(contours)
-
-
-    if biggest.size !=0:
-        biggest=functions.reorder(biggest)
-
-        #get the size of the img and the 4 points at the corner to prepare the scan
-        points = np.int32(biggest)
-        pts1 = np.float32(biggest)
-        pts1_original = pts1*5
-        
-        pts2 = np.float32([[0, 0],[width, 0], [0, height],[width, height]])
-        pts2_original = pts2 * 5
- 
-        # get a list of the 4 points a the corner 
-        point_list = functions.extract_points(points)        
-
-        matrix_original = cv2.getPerspectiveTransform(pts1_original, pts2_original)              
-        imgWarpColored_original = cv2.warpPerspective(original, matrix_original, (width_original, height_original))
-
-    card_name = predictor.predict_card_name(imgWarpColored_original)
-    print(card_name[0][0])
-
-    point_for_card_plus_name = [[x * 2 for x in sublist] for sublist in point_list]
-    card_plus_name = functions.add_card_name(card_plus_name, card_name[0][0],point_for_card_plus_name)
-    cv2.imshow('card_plus_name', card_plus_name)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 #pour chaque image dans le fichier image
 for file in os.listdir('images'):
     path_image = "images\\" +file
-    plot_card_and_name(path_image)
- 
+    scanned_card = card_detector.return_scaned_card(path_image)
+    card_name = predictor.predict_card_name(scanned_card)
+    card_detector.card_prediction(card_name) 
+
